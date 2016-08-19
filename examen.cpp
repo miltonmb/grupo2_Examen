5e916263032d55048ce97f6e57215313ce06cccb
miltonmb <milton.pasos@gmail.com>
2016-08-19 17:28:39 -0600

    Ordenado

--- examen.cpp
+++ examen.cpp
@@ -12,64 +12,66 @@ int main (){
 	std::vector<pokemon*> lista;
 	while (op != 15){
 		cout<<"1-agregar \n"<< "2-eliminar \n"<< "3-modificar \n"<< "4-buscar por nombre y naturaleza \n"<< "5-buscar por nivel \n" <<"6-buscar por tipo \n"<<"7-ordenar \n";
-		cout<<"8- buscar por hp \n"<<"9- buscar por ataque \n"<<"10-buscar por naturaleza \n"<<"11-buscar por defensa \n"<<"12- buscar por ataque especial"<<"13- buscar por defensa especial \n";
+		cout<<"8- buscar por hp \n"<<"9- buscar por ataque \n"<<"10-buscar por naturaleza \n"<<"11-buscar por defensa \n"<<"12- buscar por ataque especial\n"<<"13- buscar por defensa especial \n";
 		cout<<"14- buscar por rapidez \n"<<"15- salir \n";
 		cin>>op;
 		if (op == 1){
 			string nombre,tipo, tipo2, naturaleza, legendario;
 			double nivel, hp, defensa, ataque_especial, defensa_especial, rapidez, ataque;
 			int opcion=0;
-			cout<<"Ingrese el nombre";
+			cout<<"Ingrese el nombre: ";
 			cin>>nombre;
+			cout<<endl;
 			for (int i = 0; i < 18; i++){
 				cout<<i<<" "<<tipos[i]<<"\n";
 			}
-			cout<<"Ingrese el primer tipo";
+			cout<<"Ingrese el primer tipo: ";
 			cin>>opcion;
 			tipo = tipos[opcion];
-			cout<<"Ingrese el segundo tipo";
+			cout<<"\nIngrese el segundo tipo: ";
 			cin>>opcion;
 			tipo2 = tipos[opcion];
-			cout<<"Ingrese la naturaleza";
+			cout<<"\nIngrese la naturaleza: ";
 			for (int i = 0; i < 25; i++){
 				cout<<i<<" "<<nature[i]<<"\n";
 			}
 			cin >> opcion;
 			naturaleza = nature[opcion];
-			cout<<"es legendario? s/n";
+			cout<<"\nes legendario? s/n: ";
 			cin>>legendario;
-			cout<<"ingrese su nivel";
+			cout<<"\ningrese su nivel: ";
 			cin>>nivel;
-			cout<<"ingrese el hp";
+			cout<<"\ningrese el hp: ";
 			cin>>hp;
-			cout<<"Ingrese el ataque";
+			cout<<"\nIngrese el ataque: ";
 			cin>>ataque;
-			cout<<"Ingrese la defensa";
+			cout<<"\nIngrese la defensa: ";
 			cin>>defensa;
-			cout<<"Ingrese el ataque especial";
+			cout<<"\nIngrese el ataque especial: ";
 			cin>>ataque_especial;
-			cout<<"Ingrese la defensa especial";
+			cout<<"\nIngrese la defensa especial: ";
 			cin>>defensa_especial;
-			cout<<"Ingrese la rapidez";
+			cout<<"\nIngrese la rapidez: ";
 			cin>>rapidez;
 			pokemon* pok = new pokemon(nombre, nivel, naturaleza, tipo, tipo2, hp, ataque, defensa, ataque_especial, defensa_especial, rapidez, legendario );
 			lista.push_back(pok);
 		}
 		else if (op == 2){
 			int posicion = 0;
-			cout<<"Ingrese la posicion del pokemon";
+			cout<<"Ingrese la posicion del pokemon: ";
 			cin>>posicion;
+			cout << endl;
 			lista.erase(lista.begin()+posicion);
 		}
 		else if (op == 3){
 			string nombre;
 			double nivel;
 			int posicion;
-			cout<<"Ingrese la posicion del pokemon";
+			cout<<"\nIngrese la posicion del pokemon: ";
 			cin>>posicion;
-			cout<<"Ingrese el nuevo nombre";
+			cout<<"\nIngrese el nuevo nombre: ";
 			cin>>nombre;
-			cout<<"Ingrese el nuevo nivel";
+			cout<<"\nIngrese el nuevo nivel: ";
 			cin>>nivel;
 			pokemon* pok = lista[posicion];
 			pok -> setNombre(nombre);
@@ -80,7 +82,7 @@ int main (){
 		else if (op == 4){
 			string naturaleza, nombre, naturaleza2, nombre2;
 			pokemon* pok;
-			cout<<"Ingrese el nombre del pokemon: \n";
+			cout<<"\nIngrese el nombre del pokemon: \n";
 			cin>>nombre;
 	
 			for (int i = 0; i < lista.size(); ++i)
@@ -189,6 +191,7 @@ int main (){
 		else if(op == 6){
 			string tipo, tipo2, tip, tip2;
 			int opcion=0;
+			cout<< endl;
 			cout<<"1- buscar por un tipo: \n"<<"2- buscar por dos tipos \n";
 			cin>>opcion;
 			pokemon* pok;
@@ -208,6 +211,7 @@ int main (){
 			}
 			if (opcion==2)
 			{
+				cout << endl;
 				cout<<"Ingrese el tipo uno : \n";
 				cin>>tipo;
 				cout<<"Ingrese el tipo dos: \n";
@@ -232,6 +236,7 @@ int main (){
 			double nivel = 0;
 			double numero;
 			int opcion = 0;
+			cout <<endl;
 			pokemon* pok;
 			cout<<"buscar por: \n"<<"1-igual que \n"<<"2-diferente de \n "<<"3-menor que \n"<<"4- mayor que \n"<<"5-mayor o igual \n"<<"6- menor o igual \n";
 			cin>> opcion;
@@ -251,6 +256,7 @@ int main (){
 			}
 			if (opcion == 2)
 			{
+				cout <<endl;
 				cout<<"Ingrese el HP: \n";
 				cin>>numero;
 				for (int i = 0; i < lista.size(); ++i)
@@ -265,6 +271,7 @@ int main (){
 			}
 			if (opcion == 3)
 			{
+				cout <<endl;
 				cout<<"Ingrese el HP: \n";
 				cin>>numero;
 				for (int i = 0; i < lista.size(); ++i)
@@ -279,6 +286,7 @@ int main (){
 			}
 			if (opcion == 4)
 			{
+				cout << endl;
 				cout<<"Ingrese el HP: \n";
 				cin>>numero;
 				for (int i = 0; i < lista.size(); ++i)
@@ -293,6 +301,7 @@ int main (){
 			}
 			if (opcion == 5)
 			{
+				cout << endl;
 				cout<<"Ingrese el hp: \n";
 				cin>>numero;
 				for (int i = 0; i < lista.size(); ++i)
@@ -307,6 +316,7 @@ int main (){
 			}
 			if (opcion == 6)
 			{
+				cout << endl;
 				cout<<"Ingrese el hp: \n";
 				cin>>numero;
 				for (int i = 0; i < lista.size(); ++i)
@@ -322,10 +332,10 @@ int main (){
 		}
 		else if (op == 9)
 		{
-			/* code */
 		}
 		else if (op == 10){
-			string naturaleza, nombre, naturaleza2, nombre2;;
+			string naturaleza, nombre, naturaleza2, nombre2;
+			cout << endl;
 			cout<<"Ingrese la naturaleza: \n";
 			cin>>naturaleza;
 			pokemon* pok;
