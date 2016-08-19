5153e20b19fb5c07c10bb7662897db9876fd4ab9
gabo995 <espkh995@hotmail.com>
2016-08-19 14:30:19 -0600

    ya imprime los tipos

---
+++ examen.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream> 
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+int main (){
+	int op = 0; 
+	while (op != 7){
+		cout<<"1-agregar \n"<< "2-eliminar \n"<< "3-modificar \n"<< "4-buscar por nombre y naturaleza \n"<< "5-buscar por nivel \n" <<"6-buscar por tipo";
+		cin>>op;
+		string tipos []= {"Insecto","Dragon","Hielo","Pelea", "Fuego","Volador","Planta","Fantasma","Tierra","Electrico","Normal","Veneno","Psiquicos","Roca","Agua","Dark","Acero","Hada"};
+		if (op == 1){
+			string nombre;
+			for (int i = 0; i < 18; i++){
+				cout<<i<<" "<<tipos[i]<<"\n";
+			}
+		}
+		else if (op == 2){
+
+		}
+		else if (op == 3){
+
+		}
+		else if (op == 4){
+
+		}
+		else if (op == 5){
+
+		}
+		else if (op == 5){
+
+		}
+
+	}
+	return 0;
+}
